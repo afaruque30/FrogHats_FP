a8481899d30fcd93b1af94e8b1d197c5f06fbf10
Jason Zhou <jzhou33@stuy.edu>
2022-01-14 13:48:07 -0500

    e

---
+++ v0/Stuff.java
@@ -0,0 +1,9 @@
+import Try;
+
+public class Stuff {
+  public static void main(String[] args) {
+    System.out.println("\033[0;31m" + "▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅");
+    Try e = new Try();
+    e.so();
+  }
+}

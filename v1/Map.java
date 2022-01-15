0266cf1afccf4ad250e3c3723a79b85bf4dcb039
yang zhou <independence106@gmail.com>
2022-01-15 12:54:43 -0500

    working system so far

--- v1/Map.java
+++ v1/Map.java
@@ -1,15 +1,54 @@
+
+
 public class Map {
 
-    public 
+    private String[] map = {};
+    //should this be void or string
+    public Map() {
+        
+    }
 
+    public void clear() {
+        System.out.println("\033[H\033[2J");
+        System.out.flush();
+    }
     public void load(String[] map) {
-        String test = "";
+        this.map = new String[map.length];
         for (int i = 0; i < map.length; i++) {
-            test += map[i];
+            this.map[i] = map[i];
+        }
+    }
+
+    public void update() {
+        String test = "";
+        for (int i = 0; i < this.map.length; i++) {
             if (i % Constants.Screen.maxWidthNormal == 0) {
                 test += "\n";
             }
+            test += this.map[i];
+            
         }
         System.out.println(test);
+    }
+    //should take a Character
+    public void right() {
+        String temp = this.map[6];
+        this.map[6] = this.map[5];
+        this.map[5] = temp;
+    }
+    public static void main(String[] args) {
+        Map map = new Map();
+        String[] leve = {"h", "h", "h", "h", "h", "j", "h", "h"};
+        map.load(leve);
+        map.clear();
+        map.update();
+        try {
+            Thread.sleep(1000);
+        } catch (Exception e) {
+            System.out.println("error");
+        }
+        map.clear();
+        map.right();
+        map.update();
     }
 }

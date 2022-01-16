7ac5c912fefe7a50b4062512134ef6c52e237f44
Jason Zhou <jzhou33@stuy.edu>
2022-01-16 10:57:46 -0500

    font / things

--- v2/Menu.java
+++ v2/Menu.java
@@ -2,25 +2,25 @@ public class Menu implements Input{
     
     //Settings
     private boolean sound;
-    
+    private String display;
 
     public Menu() {
         sound = true;
     }
 
     public void loadMenu() {
-        
-        System.out.println("\t\t\t 1." + Constants.MenuDialogue.MENU_OPTION_ONE);
-        System.out.println("\n\t\t       2." + Constants.MenuDialogue.MENU_OPTION_TWO);
-        System.out.println("\n\t\t     3." + Constants.MenuDialogue.MENU_OPTION_THREE);
+        display = "\t\t\t 1." + Constants.MenuDialogue.MENU_OPTION_ONE;
+        display += "\n\t\t       2." + Constants.MenuDialogue.MENU_OPTION_TWO;
+        display += "\n\t\t     3." + Constants.MenuDialogue.MENU_OPTION_THREE;
 
     }
 
     public void update() {
-
+        System.out.println(display);
     }
     public static void main(String[] args) {
         Menu menu = new Menu();
         menu.loadMenu();
+        menu.update();
     }
 }
